a88624215a839ef02484fe443834be32b699b051
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2025-06-22 14:55:37 +0200

    fix(uploadFile)

--- StarterKit/Models/UploadFile.cs
+++ StarterKit/Models/UploadFile.cs
@@ -24,7 +24,7 @@ namespace StarterKit.Models
         public string ContentType { get; set; } = string.Empty;
 
         [Required]
-        [StringLength(50, ErrorMessage = "Image format cannot be longer than 50 characters.")]
+        [StringLength(50, ErrorMessage = "File format cannot be longer than 50 characters.")]
         public FormatEnum Format { get; set; } = FormatEnum.Png;
 
         [Required]
